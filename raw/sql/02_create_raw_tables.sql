a09346c0e040420d87dc7d7fcbab07ac2f2f80b0
Felipe Morales <dfmorales49@gmail.com>
2026-02-26 11:06:03 -0500

    Update 02_create_raw_tables.sql

--- raw/sql/02_create_raw_tables.sql
+++ raw/sql/02_create_raw_tables.sql
@@ -1,1 +1,72 @@
+-- Table: raw.clientes_csv
 
+-- DROP TABLE IF EXISTS "raw".clientes_csv;
+
+CREATE TABLE IF NOT EXISTS "raw".clientes_csv
+(
+    cliente_id text COLLATE pg_catalog."default",
+    email text COLLATE pg_catalog."default",
+    nombre text COLLATE pg_catalog."default",
+    apellido text COLLATE pg_catalog."default",
+    ciudad text COLLATE pg_catalog."default",
+    fecha_registro text COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS "raw".clientes_csv
+    OWNER to postgres;
+--------------------------------------------------------------------------------------------------------
+
+-- Table: raw.productos_csv
+
+-- DROP TABLE IF EXISTS "raw".productos_csv;
+
+CREATE TABLE IF NOT EXISTS "raw".productos_csv
+(
+    producto_id text COLLATE pg_catalog."default",
+    nombre_producto text COLLATE pg_catalog."default",
+    categoria text COLLATE pg_catalog."default",
+    precio_lista text COLLATE pg_catalog."default",
+    activo text COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS "raw".productos_csv
+    OWNER to postgres;
+------------------------------------------------------------------------------------------------------------
+
+-- Table: raw.ventas_csv
+
+-- DROP TABLE IF EXISTS "raw".ventas_csv;
+
+CREATE TABLE IF NOT EXISTS "raw".ventas_csv
+(
+    fecha text COLLATE pg_catalog."default",
+    cliente_id text COLLATE pg_catalog."default",
+    producto_id text COLLATE pg_catalog."default",
+    cantidad text COLLATE pg_catalog."default",
+    precio_unitario text COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS "raw".ventas_csv
+    OWNER to postgres;-- Table: raw.ventas_csv
+
+-- DROP TABLE IF EXISTS "raw".ventas_csv;
+
+CREATE TABLE IF NOT EXISTS "raw".ventas_csv
+(
+    fecha text COLLATE pg_catalog."default",
+    cliente_id text COLLATE pg_catalog."default",
+    producto_id text COLLATE pg_catalog."default",
+    cantidad text COLLATE pg_catalog."default",
+    precio_unitario text COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS "raw".ventas_csv
+    OWNER to postgres;
