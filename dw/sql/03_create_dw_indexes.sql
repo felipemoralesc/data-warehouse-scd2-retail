51471d1504e0e8f5d03ef381904e408dea4b736f
Felipe Morales <dfmorales49@gmail.com>
2026-03-03 13:03:12 -0500

    Update 03_create_dw_indexes.sql

--- dw/sql/03_create_dw_indexes.sql
+++ dw/sql/03_create_dw_indexes.sql
@@ -10,14 +10,19 @@ CREATE INDEX IF NOT EXISTS idx_fact_fecha
 ON dw.fact_ventas_detalle (clave_fecha);
 ---------------------------------------------------
 CREATE INDEX IF NOT EXISTS idx_dim_cliente_actual
-ON dw.dim_cliente (id_cliente_natural, es_actual);
+ON dw.dim_cliente (cliente_id, es_actual);
 
 CREATE INDEX IF NOT EXISTS idx_dim_producto_actual
-ON dw.dim_producto (id_producto_natural, es_actual);
+ON dw.dim_producto (producto_id, es_actual);
 -----------------------------------------------------
 CREATE INDEX IF NOT EXISTS idx_dim_cliente_vigencia
-ON dw.dim_cliente (id_cliente_natural, fecha_inicio_vigencia, fecha_fin_vigencia);
+ON dw.dim_cliente (cliente_id, fecha_inicio_vigencia, fecha_fin_vigencia);
 
+CREATE INDEX IF NOT EXISTS idx_dim_producto_vigencia
+ON dw.dim_producto (producto_id, fecha_inicio_vigencia, fecha_fin_vigencia);
+-----------------------------------------------------
+CREATE INDEX IF NOT EXISTS idx_dim_fecha_fecha
+ON dw.dim_fecha (fecha_completa);
 CREATE INDEX IF NOT EXISTS idx_dim_producto_vigencia
 ON dw.dim_producto (id_producto_natural, fecha_inicio_vigencia, fecha_fin_vigencia);
 -----------------------------------------------------
