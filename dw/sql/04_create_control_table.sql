cf928ba6aea212f1474786348639999d56e621ca
Felipe Morales <dfmorales49@gmail.com>
2026-03-04 13:11:46 -0500

    Update 04_create_control_table.sql

--- dw/sql/04_create_control_table.sql
+++ dw/sql/04_create_control_table.sql
@@ -1,1 +1,10 @@
-a
+CREATE TABLE dw.control_cargas (
+    id_ejecucion SERIAL PRIMARY KEY,
+    fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_fin TIMESTAMP,
+    estado VARCHAR(20),
+    registros_dim_cliente INT DEFAULT 0,
+    registros_dim_producto INT DEFAULT 0,
+    registros_fact INT DEFAULT 0,
+    mensaje_error TEXT
+);
