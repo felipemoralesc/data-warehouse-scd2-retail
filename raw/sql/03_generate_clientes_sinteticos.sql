5ad2ed2698a180863e3de0b29d4b9a1b3ebfca65
Felipe Morales <dfmorales49@gmail.com>
2026-03-05 11:59:19 -0500

    Update 03_generate_clientes_sinteticos.sql

--- raw/sql/03_generate_clientes_sinteticos.sql
+++ raw/sql/03_generate_clientes_sinteticos.sql
@@ -1,1 +1,16 @@
-a
+INSERT INTO raw.clientes_csv (
+    cliente_id,
+    nombre,
+    apellido,
+    email,
+    ciudad,
+    fecha_registro
+)
+SELECT
+    gs::text AS cliente_id,
+    ('Nombre_' || gs)::text AS nombre,
+    ('Apellido_' || gs)::text AS apellido,
+    ('cliente' || gs || '@correo.com')::text AS email,
+    (ARRAY['Bogotá','Medellín','Cali','Tunja','Barranquilla'])[floor(random()*5)+1]::text AS ciudad,
+    (CURRENT_DATE - (random()*3650)::int)::text AS fecha_registro
+FROM generate_series(1,5000) AS gs;
